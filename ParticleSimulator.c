2ffdf2401bba97478d543d4121b58f754fdb77d7
Corey Hemphill <hemphilc@oregonstate.edu>
2019-06-18 13:28:35 -0700

    Update ParticleSimulator.c

--- ParticleSimulator.c
+++ ParticleSimulator.c
@@ -3,13 +3,13 @@
 #include <stdlib.h>
 
 #define INPUT_OK      0
-#define NO_INPUT 	  1
-#define TOO_LONG 	  2
+#define NO_INPUT      1
+#define TOO_LONG      2
 #define DIM_BUFF_LEN  10
-#define RANGE_LOW	  1
-#define RANGE_HIGH 	  20
-#define SUCCESS		  0
-#define ERROR		  -1
+#define RANGE_LOW     1
+#define RANGE_HIGH    20
+#define SUCCESS       0
+#define ERROR         -1
 
 typedef struct {
     unsigned int width;
