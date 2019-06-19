14c325725864f7cc0441a6a50a8d8b9a72a6d775
hemphilc <hemphilc@oregonstate.edu>
2019-06-18 22:13:11 -0700

    Added another unit test for initParticleSimulatorGravity() function

--- testMain.c
+++ testMain.c
@@ -118,13 +118,76 @@ int main(void)
     freeParticleSimulatorGrid(&ps5);
     freeParticleSimulatorGrid(&ps5Test);
 
-    ps5.grid = NULL;
-    assert(initParticleSimulatorGravity(&ps5) == -1);
+
+		ParticleSimulator ps6;
+    unsigned int w6 = 15;
+    unsigned int h6 = 10;
+
+    assert(initParticleSimulator(&ps6, w6, h6) == 0);
+
+    strcpy(ps6.grid[0], ".........::::::");
+    ps6.grid[0][w6] = '\0';
+    strcpy(ps6.grid[1], ".T:T.T:T.T:T.T:");
+    ps6.grid[1][w6] = '\0';
+    strcpy(ps6.grid[2], "               ");
+    ps6.grid[2][w6] = '\0';
+    strcpy(ps6.grid[3], ".T .T .T .T .T ");
+    ps6.grid[3][w6] = '\0';
+		strcpy(ps6.grid[4], "               ");
+		ps6.grid[4][w6] = '\0';
+		strcpy(ps6.grid[5], "               ");
+		ps6.grid[5][w6] = '\0';
+		strcpy(ps6.grid[6], "...............");
+		ps6.grid[6][w6] = '\0';
+		strcpy(ps6.grid[7], ":::::::::::::::");
+		ps6.grid[7][w6] = '\0';
+		strcpy(ps6.grid[8], " . . . . . . . ");
+		ps6.grid[8][w6] = '\0';
+		strcpy(ps6.grid[9], " : : : : : : : ");
+		ps6.grid[9][w6] = '\0';
+
+    ParticleSimulator ps6Test;
+    assert(initParticleSimulator(&ps6Test, w6, h6) == 0);
+
+    strcpy(ps6Test.grid[0], " . . . . : : : ");
+    ps6Test.grid[0][w6] = '\0';
+    strcpy(ps6Test.grid[1], " T T T T T:T T ");
+    ps6Test.grid[1][w6] = '\0';
+    strcpy(ps6Test.grid[2], "    :     :    ");
+    ps6Test.grid[2][w6] = '\0';
+    strcpy(ps6Test.grid[3], " T  T  T  T  T ");
+    ps6Test.grid[3][w6] = '\0';
+		strcpy(ps6Test.grid[4], "               ");
+		ps6Test.grid[4][w6] = '\0';
+		strcpy(ps6Test.grid[5], "               ");
+		ps6Test.grid[5][w6] = '\0';
+		strcpy(ps6Test.grid[6], "   .  .  .  . .");
+		ps6Test.grid[6][w6] = '\0';
+		strcpy(ps6Test.grid[7], ":::: :::.: ::::");
+		ps6Test.grid[7][w6] = '\0';
+		strcpy(ps6Test.grid[8], "::::.:::::.::::");
+		ps6Test.grid[8][w6] = '\0';
+		strcpy(ps6Test.grid[9], ":::::::::::::::");
+		ps6Test.grid[9][w6] = '\0';
+
+    assert(initParticleSimulatorGravity(&ps6) == 0);
+    for(i = 0; i < h6; i++) {
+        assert(strcmp(ps6.grid[i], ps6Test.grid[i]) == 0);
+    }
+    assert(printParticleSimulatorState(ps6) == 0);
+    assert(printParticleSimulatorState(ps6Test) == 0);
     printf("TEST 2 PASSED\n\n");
 
-    ParticleSimulator *ps6 = NULL;
-    assert(initParticleSimulatorGravity(ps6) == -1);
+    freeParticleSimulatorGrid(&ps6);
+    freeParticleSimulatorGrid(&ps6Test);
+
+    ps6.grid = NULL;
+    assert(initParticleSimulatorGravity(&ps5) == -1);
     printf("TEST 3 PASSED\n\n");
+
+    ParticleSimulator *ps7 = NULL;
+    assert(initParticleSimulatorGravity(ps7) == -1);
+    printf("TEST 4 PASSED\n\n");
 
     printf("##############################################\n\n\n");
 
@@ -132,21 +195,21 @@ int main(void)
 
     printf("##### TEST freeParticleSimulatorGrid() #####\n\n");
 
-    ParticleSimulator ps7;
-    unsigned int w7 = 12;
-    unsigned int h7 = 16;
+    ParticleSimulator ps8;
+    unsigned int w8 = 12;
+    unsigned int h8 = 16;
 
-    assert(initParticleSimulator(&ps7, w7, h7) == 0);
-    assert(ps7.grid != NULL);
-    freeParticleSimulatorGrid(&ps7);
-    assert(ps7.grid == NULL);
+    assert(initParticleSimulator(&ps8, w8, h8) == 0);
+    assert(ps8.grid != NULL);
+    freeParticleSimulatorGrid(&ps8);
+    assert(ps8.grid == NULL);
     printf("TEST 1 PASSED\n\n");
 
-    freeParticleSimulatorGrid(&ps7);
+    freeParticleSimulatorGrid(&ps8);
     printf("TEST 2 PASSED\n\n");
 
-    ParticleSimulator *ps8 = NULL;
-    freeParticleSimulatorGrid(ps8);
+    ParticleSimulator *ps9 = NULL;
+    freeParticleSimulatorGrid(ps9);
     printf("TEST 3 PASSED\n\n");
 
     printf("##############################################\n\n\n");
